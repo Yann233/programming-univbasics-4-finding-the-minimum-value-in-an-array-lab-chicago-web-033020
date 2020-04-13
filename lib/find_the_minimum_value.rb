77597fb209a695e6829268cd3d942a5c47be6f22
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 06:45:18 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,15 +1,3 @@
 def find_min_value(array)
 array.min
 end
-
-#def find_min_value(array)
-#   i = 0
-#   max_value = 100
-#   while i < array.length do
-#     if max_value < array[i]
-#       max_value = array[i]
-#     end
-#     i += 1
-#   end
-#   max_value
-# end
